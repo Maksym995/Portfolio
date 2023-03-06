e2d0b974707de76aeb52f1e08942cc27e062aba2
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2023-03-06 12:35:19 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+require_once "../config.php";
+require_once "../model/ContactModel.php";
+require_once "../model/LiensModel.php";
+require_once "../model/TravauxModel.php";
+//require_once "../model/UtilisateursModel.php";
+
+# Connexion
+try{
+    $db = mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+    mysqli_set_charset($db,DB_CHARSET);
+}catch(Exception $e){
+    exit($e->getMessage());
+}
+
+# Router
+if(false){
+    require_once "../controller/privateController.php";
+  
+}else{
+    require_once "../controller/publicController.php";
+}
+
+mysqli_close($db);
---
+++ view/publicView/404View.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Maksym</title>
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+
+<nav class="">
+
+</nav>
+
+<div class="">
+    <div class="">
+        <div class="">
+                <header class="">
+                    <h1 class=""><?=$error?></h1>
+                    <h3>Erreur 404 : <?=$error?> <a href="./">Revenir à l'accueil</a></h3>
+                </header>
+                
+
+</div>
+
+<footer class="">
+    <div class=""><p class="">Copyright &copy; Your Website 2022</p></div>
+</footer>
+
+</body>
+</html>
